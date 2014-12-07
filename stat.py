8ecb0ea522d3e65a94d498c3149754755a2a4f25
Jackon Yang <jiekunyang@gmail.com>
2014-12-07 13:28:31 +0800

    freq

--- stat.py
+++ stat.py
@@ -17,11 +17,13 @@ def sort_dict(orig):
 
 def prob(item):
     res = dict()
+
     def add(ch):
         if ch in res:
             res[ch] += 1
         else:
             res[ch] = 1
+
     for ch in item:
         add(ch)
     return [res['A'], res['B'], res['C'], res['D']]
@@ -33,8 +35,27 @@ def outline(data):
             '\tcloze: ', prob(answer[:20]),\
             '\tread_a: ', prob(answer[20:40])
 
+@print_cutting_line
+def freq(data):
+    count = dict()
+
+    def add(ch, i):
+        def _wrap():
+            if ch not in count:
+                count[ch] = [0] * len(answer)
+            count[ch][i] += 1
+        _wrap()
+
+    for answer in data.values():
+        for i in range(len(answer)):
+            add(answer[i], i)
+
+    for answer, fq in sort_dict(count):
+        print answer, fq
+
 def run():
     outline(read_data('data/data.txt'))
+    freq(read_data('data/data.txt'))
 
 if __name__ == "__main__":
     run()
